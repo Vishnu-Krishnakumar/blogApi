77027da6f487d575f8a0aadba0cdad6db3206bcb
Vishnu-Krishnakumar <vkrish3@wgu.edu>
2025-05-07 14:36:11 +0530

    changed cors settings

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173","https://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
     credentials: true,
   })
 );
